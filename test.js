4a21d97854d2fc0176b325ac7b9bcfaf10242289
donnamccabe97 <donnasarahmccabe@eircom.net>
2019-04-12 12:34:55 +0100

    Written test first - Failing

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 const assert = require('assert')
 
-it('should return true', () => {
-  assert.equal(true, true)
+it('correctly calculates the sum of 1 and 3', () => {
+  assert.equal(add(1, 3), 4)
 })
